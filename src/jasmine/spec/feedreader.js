7b5cf69f501a11c99e75e14506bcaa96688b4761
Juan Manuel Sánchez <juanmanuel.sanchez@icloud.com>
2015-06-14 00:36:21 +0200

    Add comments to the `RSS feed` and `The menu` specs

--- src/jasmine/spec/feedreader.js
+++ src/jasmine/spec/feedreader.js
@@ -9,203 +9,198 @@
  * to ensure they don't run until the DOM is ready.
  */
 $(function() {
-  /* This is our first test suite - a test suite just contains
-   * a related set of tests. This suite is all about the RSS
-   * feeds definitions, the allFeeds variable in our application.
-   */
-  describe('RSS Feeds', function() {
+
     /* This is our first test - it tests to make sure that the
      * allFeeds variable has been defined and that it is not
      * empty.
-     * In this case we're iterating through allFeeds JSON
+     * In this case we're going to iterate through allFeeds JSON
      * to help verifying that RSS Feeds "are defined" and
      * RSS Feeds "url are defined".
      */
-    var feed,
-        url,
-        name,
-        i=0;
+
+     describe('RSS Feeds', function() {
 
 
-    /* To help the test suite to DRY up any repeated
-     * setup we're using afterEach functions
-     *
-     */
-    afterEach(function() {
-
-      feed=null;
-      url=null;
-      name=null;
-      i=0;
-
-    });
+        /* Declares the variables we're using to iterate
+         * through allFeeds JSON.
+         */
+         var feed,
+             url,
+             name,
+             i=0;
 
 
-    it('and all feeds-Url are defined', function() {
+       /* To help the test suite to DRY up any repeated
+        * setup we're using afterEach function.
+        * Restores the variables to their original state.
+        */
+        afterEach(function() {
 
-      for(i; i<allFeeds.length; i++) {
-
-        feed=allFeeds[i];
-        url=feed.url;
-
-        expect(feed).toBeDefined();
-        expect(allFeeds.length).not.toBe(0);
-        expect(url).toBeDefined();
-        expect(url).not.toBeNull();
-        console.log(url);
-
-      }
-
-    });
-
-
-    it('and all feeds-names are defined', function() {
-
-      for(i; i<allFeeds.length; i++) {
-
-        feed=allFeeds[i];
-        name=feed.name;
-
-        expect(name).toBeDefined();
-        expect(name).not.toBeNull();
-        console.log(name);
-
-      }
-
-    });
-
-
-    /* TODO: Write a test that loops through each feed
-     * in the allFeeds object and ensures it has a URL defined
-     * and that the URL is not empty.
-     * All the url provided in allFeeds JSON are defined and
-     * different from null.
-     */
-
-
-
-
-    /* TODO: Write a test that loops through each feed
-     * in the allFeeds object and ensures it has a name defined
-     * and that the name is not empty.
-     * All the names provided in allFeeds JSON are defined and
-     * different from null.
-     */
-
-  });
-
-
-  /* TODO: Write a new test suite named "The menu" */
-
-  /* TODO: Write a test that ensures the menu element is
-   * hidden by default. You'll have to analyze the HTML and
-   * the CSS to determine how we're performing the
-   * hiding/showing of the menu element.
-   *
-   * In this case describe blocks are nested with specs defined
-   * at different levels.
-   */
-
-  describe('The menu', function() {
-
-    it('is hidden by default ', function () {
-
-
-      expect($('body').hasClass('menu-hidden')).toBeTruthy();
-
-
-    });
-
-
-    describe('displays and hides when clicked', function() {
-
-
-      var menuIcon = $('.menu-icon-link');
-
-
-      it('displays when clicked', function () {
-
-        $(menuIcon).trigger( "click" );
-        console.log("Click and displays!");
-        expect($('body').hasClass('menu-hidden')).toBeFalsy();
-
-      });
-
-      it('hides when clicked again', function () {
-
-        $(menuIcon).trigger( "click" );
-        console.log("Click and hides!");
-        expect($('body').hasClass('menu-hidden')).toBeTruthy();
-
-      });
-
-    });
-
-  });
-
-
-
-
-  describe('New feed selection', function() {
-
-
-    var firstFeedName,
-        secondFeedName,
-        defaultFeedName,
-        firstFeedId=0;
-        secondFeedId=1,
-        firstFeedTitle=null,
-        secondFeedTitle=null;
-
-
-
-    beforeEach(function(done) {
-
-      loadFeed(firstFeedId,function() {
-
-        loadFeed(secondFeedId, function() {
-
-          secondFeedName=allFeeds[secondFeedId].name;
-
-          done();
+            feed,
+            url,
+            name,
+            i=0;
 
         });
-      });
-    });
 
-    afterAll(function(callback){
+       /* Checks allFeeds JSON the url property provided
+        * in allFeeds JSON are defined and different from null.
+        * We're iterating through allFeeds JSON with a for loop.
+        *
+        */
+        it('and allFeeds-url are defined', function() {
 
-      loadFeed(firstFeedId, function() {
+            for(i; i<allFeeds.length; i++) {
+                feed=allFeeds[i];
+                url=feed.url;
 
-        callback();
+                expect(feed).toBeDefined();
+                expect(allFeeds.length).not.toBe(0);
+                expect(url).toBeDefined();
+                expect(url).not.toBeNull();
 
-      });
-    });
+            }
 
+        });
 
-    it('produces new content', function (done) {
+       /* Checks that the name property provided in allFeeds
+        * JSON is defined and different from null.
+        * We're iterating through allFeeds JSON with a for loop.
+        */
+        it('and allFeeds-name are defined', function() {
 
-      firstFeedName=allFeeds[firstFeedId].name;
-      expect(firstFeedName).toMatch('Udacity Blog');
-      expect(secondFeedName).toMatch('CSS Tricks');
-      console.log(firstFeedName);
-      console.log(secondFeedName);
-      expect(firstFeedName).not.toEqual(secondFeedName);
+            for(i; i<allFeeds.length; i++) {
 
-      done();
+                feed=allFeeds[i];
+                name=feed.name;
 
-    });
+                expect(name).toBeDefined();
+                expect(name).not.toBeNull();
 
-    it('restores the loadFeed to the default', function (callback) {
+            }
 
-      defaultFeedName= allFeeds[firstFeedId].name;
-      expect(defaultFeedName).toMatch('Udacity Blog');
-      console.log(defaultFeedName);
-
-      callback();
-    });
+        });
 
   });
+
+
+
+   /* This is our second test suite and tests the interactive
+    * nature of the menu.
+    */
+    describe('The menu', function() {
+
+       /* Checks that menu is hidden by default with JQuery .hasClass()
+        * method. The 'menu-hidden' class evaluates to true on the body
+        * by default.
+        */
+        it('is hidden by default ', function () {
+
+           expect($('body').hasClass('menu-hidden')).toBeTruthy();
+
+        });
+
+       /* Checks that menu displays when clicking .menu-icon-link
+        * with JQuery .trigger() to perform a click event, and
+        * .hasClass() methods.
+        */
+
+        describe('displays and hides when clicked', function() {
+
+            //Holds the menu-icon-link of the DOM
+            var menuIcon = $('.menu-icon-link');
+
+           /* Evaluates to  false the presence of 'menu-hidden' class
+            * in the body when clicking the menu-icon-link through
+            * JQuery .trigger() method.
+            */
+            it('displays when clicked', function () {
+
+                //performs a click event
+                $(menuIcon).trigger( "click" );
+                expect($('body').hasClass('menu-hidden')).toBeFalsy();
+
+            });
+
+          /* Evaluates to true the presence of 'menu-hidden' class in
+           * the body when clicking again the menu-icon-link through
+           * JQuery .trigger() method.
+           */
+           it('hides when clicked again', function () {
+
+               //performs a click event
+               $(menuIcon).trigger( "click" );
+               expect($('body').hasClass('menu-hidden')).toBeTruthy();
+
+           });
+
+        });
+
+    });
+
+
+
+
+    describe('New feed selection', function() {
+
+
+        var firstFeedName,
+            secondFeedName,
+            defaultFeedName,
+            firstFeedId=0;
+            secondFeedId=1,
+            firstFeedTitle=null,
+            secondFeedTitle=null;
+
+
+
+        beforeEach(function(done) {
+
+            loadFeed(firstFeedId,function() {
+
+                loadFeed(secondFeedId, function() {
+
+                    secondFeedName=allFeeds[secondFeedId].name;
+
+                    done();
+
+                });
+            });
+        });
+
+        afterAll(function(callback){
+
+           loadFeed(firstFeedId, function() {
+
+               callback();
+
+           });
+        });
+
+
+        it('produces new content', function (done) {
+
+            firstFeedName=allFeeds[firstFeedId].name;
+            expect(firstFeedName).toMatch('Udacity Blog');
+            expect(secondFeedName).toMatch('CSS Tricks');
+            console.log(firstFeedName);
+            console.log(secondFeedName);
+            expect(firstFeedName).not.toEqual(secondFeedName);
+
+            done();
+
+        });
+
+        it('restores the loadFeed to the default', function (callback) {
+
+            defaultFeedName= allFeeds[firstFeedId].name;
+            expect(defaultFeedName).toMatch('Udacity Blog');
+            console.log(defaultFeedName);
+
+            callback();
+        });
+
+    });
 
 
 }());
